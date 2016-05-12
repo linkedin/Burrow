5d95fbd6cd319926f072c36ca5d21c0900034bd8
tpalino <tpalino@linkedin.com>
2016-05-12 11:33:58 -0700

    Add Travis config and a single dummy test

---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+)
+
+// This is just a dummy test function to have a base to start with for Travis
+func Test_dummy(t *testing.T) {
+	t.Log("Dummy test passed")
+}
