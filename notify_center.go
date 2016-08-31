e12d930bb740d6a26682e0d2b3f04edde36178d5
Martin Sucha <martin.sucha@exponea.com>
2016-08-31 20:04:25 +0200

    Allow more generic HTTP client configuration
    Allow the user to specify a different URL and HTTP method to close
    alerts. Renamed POST/DELETE to be open/close as the HTTP method is now
    configurable. The configuration file variable names still use `delete`
    suffix instead of `close` to stay backwards compatible with the original
    config file format.

--- notify_center.go
+++ notify_center.go
@@ -36,7 +36,7 @@ type NotifyCenter struct {
 
 func LoadNotifiers(app *ApplicationContext) error {
 	notifiers := []notifier.Notifier{}
-	if app.Config.Httpnotifier.Url != "" {
+	if app.Config.Httpnotifier.UrlOpen != "" {
 		if httpNotifier, err := NewHttpNotifier(app); err == nil {
 			notifiers = append(notifiers, httpNotifier)
 		}
@@ -220,11 +220,18 @@ func NewHttpNotifier(app *ApplicationContext) (*notifier.HttpNotifier, error) {
 	}
 
 	return &notifier.HttpNotifier{
-		Url:                httpConfig.Url,
+		RequestOpen: notifier.HttpNotifierRequest{
+			Url:          httpConfig.UrlOpen,
+			Method:       httpConfig.MethodOpen,
+			TemplateFile: httpConfig.TemplateOpen,
+		},
+		RequestClose: notifier.HttpNotifierRequest{
+			Url:          httpConfig.UrlClose,
+			Method:       httpConfig.MethodClose,
+			TemplateFile: httpConfig.TemplateClose,
+		},
 		Threshold:          httpConfig.PostThreshold,
-		SendDelete:         httpConfig.SendDelete,
-		TemplatePostFile:   httpConfig.TemplatePost,
-		TemplateDeleteFile: httpConfig.TemplateDelete,
+		SendClose:          httpConfig.SendClose,
 		Extras:             extras,
 		HttpClient: &http.Client{
 			Timeout: time.Duration(httpConfig.Timeout) * time.Second,
