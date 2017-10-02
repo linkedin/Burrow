6a622fcb10e632f8e3f746a5228b23e51fe4d59c
Jim Plush <jiminoc@gmail.com>
2017-10-02 16:56:52 -0700

    had a double lock, only needed on public func

--- notifier/http_notifier.go
+++ notifier/http_notifier.go
@@ -21,6 +21,7 @@ import (
 	"text/template"
 	"time"
 	"fmt"
+	"sync"
 )
 
 type HttpNotifier struct {
@@ -31,9 +32,11 @@ type HttpNotifier struct {
 	Extras             map[string]string
 	HttpClient         *http.Client
 	groupIds           map[string]map[string]Event
+	sync.Mutex
 }
 
 type HttpNotifierRequest struct {
+	sync.Mutex
 	Url string
 	TemplateFile string
 	Method string
@@ -50,6 +53,8 @@ func (notify *HttpNotifier) NotifierName() string {
 }
 
 func (notifier *HttpNotifier) Notify(msg Message) error {
+	notifier.Lock()
+	defer notifier.Unlock()
 	// Helper functions for templates
 	fmap := template.FuncMap{
 		"jsonencoder":     templateJsonEncoder,
