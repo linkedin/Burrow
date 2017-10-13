eef726899e34e342a6c404cc332f8c1a23b479b2
Todd Palino <tpalino@linkedin.com>
2017-10-13 11:37:42 -0400

    Add start/stop and multiple request tests for the evaluator coordinator

--- core/internal/evaluator/coordinator_test.go
+++ core/internal/evaluator/coordinator_test.go
@@ -59,28 +59,54 @@ func TestCoordinator_Configure_TwoModules(t *testing.T) {
 	assert.Panics(t, coordinator.Configure, "Expected panic")
 }
 
-/*
 func TestCoordinator_Start(t *testing.T) {
-	coordinator := fixtureCoordinator()
-	coordinator.Configure()
-	coordinator.Start()
+	evaluatorCoordinator, storageCoordinator := StorageAndEvaluatorCoordinatorsWithOffsets()
 
 	// Best is to test a request that we know the response to
-	request := &protocol.StorageRequest{
-		RequestType: protocol.StorageFetchClusters,
-		Reply:       make(chan interface{}),
+	request := &protocol.EvaluatorRequest{
+		Reply:   make(chan *protocol.ConsumerGroupStatus),
+		Cluster: "testcluster",
+		Group:   "testgroup",
+		ShowAll: true,
+
 	}
-	coordinator.App.StorageChannel <- request
+	evaluatorCoordinator.App.EvaluatorChannel <- request
 	response := <- request.Reply
 
-	assert.IsType(t, []string{}, response, "Expected response to be of type []string")
-	val := response.([]string)
-	assert.Len(t, val, 1, "One entry not returned")
-	assert.Equalf(t, val[0], "testcluster", "Expected return value to be 'testcluster', not %v", val[0])
+	assert.Equalf(t, protocol.StatusOK, response.Status, "Expected status to be OK, not %v", response.Status.String())
+	assert.True(t, response.Complete, "Expected complete to be true")
+	assert.Equalf(t, 1, response.TotalPartitions, "Expected total_partitions to be 1, not %v", response.TotalPartitions)
+	assert.Equalf(t, uint64(2421), response.TotalLag, "Expected total_lag to be 2421, not %v", response.TotalLag)
+	assert.Equalf(t, "testcluster", response.Cluster, "Expected cluster to be testcluster, not %v", response.Cluster)
+	assert.Equalf(t, "testgroup", response.Group, "Expected group to be testgroup, not %v", response.Group)
+	assert.Lenf(t, response.Partitions, 1, "Expected 1 partition status objects, not %v", len(response.Partitions))
 
 	response, ok := <- request.Reply
 	assert.False(t, ok, "Expected channel to be closed")
 
-	coordinator.Stop()
+	evaluatorCoordinator.Stop()
+	storageCoordinator.Stop()
 }
-*/
+
+func TestCoordinator_MultipleRequests(t *testing.T) {
+	evaluatorCoordinator, storageCoordinator := StorageAndEvaluatorCoordinatorsWithOffsets()
+
+	// This test is really just to check and make sure the evaluator can handle multiple requests without deadlock
+	for i:= 0; i < 10; i++ {
+		request := &protocol.EvaluatorRequest{
+			Reply:   make(chan *protocol.ConsumerGroupStatus),
+			Cluster: "testcluster",
+			Group:   "testgroup",
+			ShowAll: true,
+
+		}
+		evaluatorCoordinator.App.EvaluatorChannel <- request
+		response := <- request.Reply
+
+		assert.Equalf(t, protocol.StatusOK, response.Status, "Expected status to be OK, not %v", response.Status.String())
+	}
+	// Best is to test a request that we know the response to
+
+	evaluatorCoordinator.Stop()
+	storageCoordinator.Stop()
+}
