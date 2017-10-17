7cf3a8e3395e871f6da8226f29f6dbf728477f0c
Todd Palino <tpalino@linkedin.com>
2017-10-17 13:49:55 -0700

    Add tests for the rest of the kafka cluster module

--- core/internal/cluster/kafka_cluster_test.go
+++ core/internal/cluster/kafka_cluster_test.go
@@ -1,6 +1,7 @@
 package cluster
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/linkedin/Burrow/core/configuration"
@@ -9,6 +10,9 @@ import (
 	"go.uber.org/zap"
 	"github.com/stretchr/testify/assert"
 	"github.com/linkedin/Burrow/core/internal/helpers"
+	"github.com/Shopify/sarama"
+	"github.com/stretchr/testify/mock"
+	"time"
 )
 
 func fixtureModule() *KafkaCluster {
@@ -109,4 +113,124 @@ func TestKafkaCluster_maybeUpdateMetadataAndDeleteTopics_Delete(t *testing.T) {
 	assert.Equalf(t, protocol.StorageSetDeleteTopic, request.RequestType, "Expected request sent with type StorageSetDeleteTopic, not %v", request.RequestType)
 	assert.Equalf(t, "test", request.Cluster, "Expected request sent with cluster test, not %v", request.Cluster)
 	assert.Equalf(t, "topictodelete", request.Topic, "Expected request sent with topic topictodelete, not %v", request.Topic)
+}
+
+func TestKafkaCluster_generateOffsetRequests(t *testing.T) {
+	module := fixtureModule()
+	module.Configure("test")
+	module.topicMap = make(map[string]int)
+	module.topicMap["testtopic"] = 1
+
+	// Set up a broker mock
+	broker := &helpers.MockSaramaBroker{}
+	broker.On("ID").Return(int32(13))
+
+	// Set up the mock to return the leader broker for a test topic and partition
+	client := &helpers.MockSaramaClient{}
+	client.On("Leader", "testtopic", int32(0)).Return(broker, nil)
+
+	requests, brokers := module.generateOffsetRequests(client)
+
+	broker.AssertExpectations(t)
+	client.AssertExpectations(t)
+	assert.Lenf(t, brokers, 1, "Expected 1 broker entry, not %v", len(brokers))
+	_, ok := brokers[13]
+	assert.True(t, ok, "Expected key for the broker to be its ID")
+	assert.Equal(t, broker, brokers[13], "Expected broker returned to be the mock")
+	assert.Lenf(t, requests, 1, "Expected 1 request, not %v", len(requests))
+}
+
+func TestKafkaCluster_generateOffsetRequests_NoLeader(t *testing.T) {
+	module := fixtureModule()
+	module.Configure("test")
+	module.topicMap = make(map[string]int)
+	module.topicMap["testtopic"] = 2
+
+	// Set up a broker mock
+	broker := &helpers.MockSaramaBroker{}
+	broker.On("ID").Return(int32(13))
+
+	// Set up the mock to return the leader broker for a test topic and partition
+	client := &helpers.MockSaramaClient{}
+	var nilBroker *helpers.SaramaBroker
+	client.On("Leader", "testtopic", int32(0)).Return(nilBroker, errors.New("no leader error"))
+	client.On("Leader", "testtopic", int32(1)).Return(broker, nil)
+
+	requests, brokers := module.generateOffsetRequests(client)
+
+	broker.AssertExpectations(t)
+	client.AssertExpectations(t)
+	assert.Lenf(t, brokers, 1, "Expected 1 broker entry, not %v", len(brokers))
+	_, ok := brokers[13]
+	assert.True(t, ok, "Expected key for the broker to be its ID")
+	assert.Equal(t, broker, brokers[13], "Expected broker returned to be the mock")
+	assert.Lenf(t, requests, 1, "Expected 1 request, not %v", len(requests))
+}
+
+func TestKafkaCluster_getOffsets(t *testing.T) {
+	module := fixtureModule()
+	module.Configure("test")
+	module.topicMap = make(map[string]int)
+	module.topicMap["testtopic"] = 2
+	module.fetchMetadata = false
+
+	// Set up an OffsetResponse
+	offsetResponse := &sarama.OffsetResponse{Version: 1}
+	offsetResponse.AddTopicPartition("testtopic", 0, 8374)
+
+	// Set up a broker mock
+	broker := &helpers.MockSaramaBroker{}
+	broker.On("ID").Return(int32(13))
+	broker.On("GetAvailableOffsets", mock.MatchedBy(func(request *sarama.OffsetRequest) bool { return request != nil })).Return(offsetResponse, nil)
+
+	// Set up the mock to return the leader broker for a test topic and partition
+	client := &helpers.MockSaramaClient{}
+	var nilBroker *helpers.SaramaBroker
+	client.On("Leader", "testtopic", int32(0)).Return(broker, nil)
+	client.On("Leader", "testtopic", int32(1)).Return(nilBroker, errors.New("no leader error"))
+
+	go module.getOffsets(client)
+	request := <- module.App.StorageChannel
+
+	broker.AssertExpectations(t)
+	client.AssertExpectations(t)
+	assert.Equalf(t, protocol.StorageSetBrokerOffset, request.RequestType, "Expected request sent with type StorageSetBrokerOffset, not %v", request.RequestType)
+	assert.Equalf(t, "test", request.Cluster, "Expected request sent with cluster test, not %v", request.Cluster)
+	assert.Equalf(t, "testtopic", request.Topic, "Expected request sent with topic testtopic, not %v", request.Topic)
+	assert.Equalf(t, int32(0), request.Partition, "Expected request sent with partition 0, not %v", request.Partition)
+	assert.Equalf(t, int32(2), request.TopicPartitionCount, "Expected request sent with TopicPartitionCount 2, not %v", request.TopicPartitionCount)
+	assert.Equalf(t, int64(8374), request.Offset, "Expected request sent with offset 8374, not %v", request.Offset)
+
+	// Make sure there is nothing else on the channel
+	time.Sleep(100 * time.Millisecond)
+	select {
+	case <- module.App.StorageChannel:
+		t.Fatal("Expected no additional value waiting on storage channel")
+	default:
+		break
+	}
+}
+
+func TestKafkaCluster_getOffsets_BrokerFailed(t *testing.T) {
+	module := fixtureModule()
+	module.Configure("test")
+	module.topicMap = make(map[string]int)
+	module.topicMap["testtopic"] = 1
+	module.fetchMetadata = false
+
+	// Set up a broker mock
+	broker := &helpers.MockSaramaBroker{}
+	broker.On("ID").Return(int32(13))
+	var offsetResponse *sarama.OffsetResponse
+	broker.On("GetAvailableOffsets", mock.MatchedBy(func(request *sarama.OffsetRequest) bool { return request != nil })).Return(offsetResponse, errors.New("broker failed"))
+	broker.On("Close").Return(nil)
+
+	// Set up the mock to return the leader broker for a test topic and partition
+	client := &helpers.MockSaramaClient{}
+	client.On("Leader", "testtopic", int32(0)).Return(broker, nil)
+
+	module.getOffsets(client)
+
+	broker.AssertExpectations(t)
+	client.AssertExpectations(t)
 }
