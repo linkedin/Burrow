d553e868a128190f82c3bf6ae3d691f9659287ee
Dennis Ordanov <dennis@robinhood.com>
2018-02-01 17:13:27 -0800

    Update sarama to support 10.2.1 too.

--- core/internal/helpers/sarama.go
+++ core/internal/helpers/sarama.go
@@ -38,7 +38,7 @@ func parseKafkaVersion(kafkaVersion string) sarama.KafkaVersion {
 		return sarama.V0_10_0_1
 	case "0.10.1", "0.10.1.0":
 		return sarama.V0_10_1_0
-	case "", "0.10.2", "0.10.2.0":
+	case "", "0.10.2", "0.10.2.0", "0.10.2.1":
 		return sarama.V0_10_2_0
 	case "0.11.0", "0.11.0.1", "0.11.0.2":
 		return sarama.V0_11_0_0
