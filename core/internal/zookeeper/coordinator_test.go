82718c5a0d6a7d7d6892b9be10c4b7d7c67556c3
yumHunter <luyichunter@gmail.com>
2018-07-11 07:53:59 -0500

    Add zookeeper coordinator test on Exists method

--- core/internal/zookeeper/coordinator_test.go
+++ core/internal/zookeeper/coordinator_test.go
@@ -65,8 +65,11 @@ func TestCoordinator_StartStop(t *testing.T) {
 		return &mockClient, eventChan, nil
 	}
 
-	mockClient.On("Create", "/test", []byte{}, int32(0), zk.WorldACL(zk.PermAll)).Return("", zk.ErrNodeExists)
-	mockClient.On("Create", "/test/path", []byte{}, int32(0), zk.WorldACL(zk.PermAll)).Return("", zk.ErrNodeExists)
+	mockClient.On("Exists", "/test").Return(true,"","")
+	mockClient.On("Exists", "/test/path").Return(true,"","")
+	mockClient.On("Exists", "/test/path").Return(false,"","")
+	// mockClient.On("Create", "/test", []byte{}, int32(0), zk.WorldACL(zk.PermAll)).Return("", zk.ErrNodeExists)
+	// mockClient.On("Create", "/test/path", []byte{}, int32(0), zk.WorldACL(zk.PermAll)).Return("", zk.ErrNodeExists)
 	mockClient.On("Create", "/test/path/burrow", []byte{}, int32(0), zk.WorldACL(zk.PermAll)).Return("", nil)
 	mockClient.On("Close").Run(func(args mock.Arguments) { close(eventChan) }).Return()
 
