efd12cf6854b529e44022b1a9554625e8ebe2518
yumHunter <luyichunter@gmail.com>
2018-07-12 10:21:59 -0500

    Update zookeeper coordinator test - Exists return type

--- core/internal/zookeeper/coordinator_test.go
+++ core/internal/zookeeper/coordinator_test.go
@@ -65,9 +65,9 @@ func TestCoordinator_StartStop(t *testing.T) {
 		return &mockClient, eventChan, nil
 	}
 
-	mockClient.On("Exists", "/test").Return(true, "", nil)
-	mockClient.On("Exists", "/test/path").Return(true, "", nil)
-	mockClient.On("Exists", "/test/path/burrow").Return(false, "", nil)
+	mockClient.On("Exists", "/test").Return(true, []zk.Stat, nil)
+	mockClient.On("Exists", "/test/path").Return(true, []zk.Stat, nil)
+	mockClient.On("Exists", "/test/path/burrow").Return(false, nil, nil)
 	// mockClient.On("Create", "/test", []byte{}, int32(0), zk.WorldACL(zk.PermAll)).Return("", zk.ErrNodeExists)
 	// mockClient.On("Create", "/test/path", []byte{}, int32(0), zk.WorldACL(zk.PermAll)).Return("", zk.ErrNodeExists)
 	mockClient.On("Create", "/test/path/burrow", []byte{}, int32(0), zk.WorldACL(zk.PermAll)).Return("", nil)
