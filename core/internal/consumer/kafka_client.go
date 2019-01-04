5a26d455d201c47e675b6b70ba55bab4f01e1bdf
Mario Longobardi <mlongobardi2@bloomberg.net>
2019-01-04 13:27:45 -0500

    Only process metadata for groups with ProtocolType = consumer

--- core/internal/consumer/kafka_client.go
+++ core/internal/consumer/kafka_client.go
@@ -418,6 +418,11 @@ func (module *KafkaClient) decodeAndSendGroupMetadata(valueVersion int16, group 
 		)
 		return
 	}
+	metadataLogger.Debug("group metadata")
+	if metadataHeader.ProtocolType != "consumer" {
+		metadataLogger.Debug("skipped metadata because of unknown protocolType")
+		return
+	}
 
 	var memberCount int32
 	err := binary.Read(valueBuffer, binary.BigEndian, &memberCount)
@@ -449,7 +454,6 @@ func (module *KafkaClient) decodeAndSendGroupMetadata(valueVersion int16, group 
 			return
 		}
 
-		metadataLogger.Debug("group metadata")
 		for topic, partitions := range member.Assignment {
 			for _, partition := range partitions {
 				helpers.TimeoutSendStorageRequest(module.App.StorageChannel, &protocol.StorageRequest{
