238f1834bf39b20487672a4fc290ce72011af33d
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-07-06 20:13:36 +0530

    drop

--- run-burrow.sh
+++ run-burrow.sh
@@ -2,7 +2,7 @@ set +x
 
 if [ -z "$1" ]
 then
-    do /app/burrow --config-dir $1
+    /app/burrow --config-dir $1
 else
-    do /app/burrow --config-dir /etc/burrow
+    /app/burrow --config-dir /etc/burrow
 fi
