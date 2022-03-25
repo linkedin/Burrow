f3f34226905ac4d6f787a9b6bd3e12e608552de1
muicoder <muicoder@gmail.com>
2022-03-26 01:13:14 +0800

    Add test data for notifier

--- core/internal/notifier/coordinator_test.go
+++ core/internal/notifier/coordinator_test.go
@@ -472,6 +472,7 @@ var notifyModuleTests = []struct {
 	ExpectClose bool
 	ExpectID    bool
 	SendOnce    bool
+	Cluster string
 }{
 	// {1, 0, false, false, false, false, false},
 	// {2, 0, false, false, false, false, false},
@@ -479,27 +480,31 @@ var notifyModuleTests = []struct {
 	// {1, 0, false, true, false, false, false},
 	// {1, 0, true, true, false, false, false},
 
-	{1, 1, false, false, true, false, false, false},
-	{1, 1, false, true, true, false, false, false},
-	{1, 1, true, false, true, false, false, false},
-	{1, 1, true, true, true, true, false, true},
+	{1, 1, false, false, true, false, false, false, ""},
+	{1, 1, false, true, true, false, false, false, "testcluster"},
+	{1, 1, true, false, true, false, false, false, "unmatchedCluster"},
+	{1, 1, true, true, true, true, false, true, ""},
 
-	{1, 2, false, false, true, false, true, false},
-	{1, 2, false, true, true, false, true, false},
-	{1, 2, true, false, true, false, true, false},
-	{1, 2, true, true, true, false, true, false},
-	{1, 2, true, true, false, false, true, true},
-	{1, 2, false, true, true, false, true, true},
+	{1, 2, false, false, true, false, true, false, ""},
+	{1, 2, false, true, true, false, true, false, ""},
+	{1, 2, true, false, true, false, true, false, ""},
+	{1, 2, true, true, true, false, true, false, ""},
+	{1, 2, true, true, false, false, true, true, ""},
+	{1, 2, false, true, true, false, true, true, ""},
 
-	{3, 2, false, false, false, false, true, false},
-	{3, 2, false, true, false, false, true, false},
-	{3, 2, true, false, false, false, true, false},
-	{3, 2, true, true, false, false, true, false},
+	{3, 2, false, false, false, false, true, false, ""},
+	{3, 2, false, true, false, false, true, false, ""},
+	{3, 2, true, false, false, false, true, false, ""},
+	{3, 2, true, true, false, false, true, false, ""},
 
-	{2, 1, false, false, false, false, false, false},
-	{2, 1, false, true, false, false, false, false},
-	{2, 1, true, false, false, false, false, false},
-	{2, 1, true, true, true, true, false, false},
+	{2, 1, false, false, false, false, false, false, ""},
+	{2, 1, false, true, false, false, false, false, ""},
+	{2, 1, true, false, false, false, false, false, ""},
+	{2, 1, true, true, true, true, false, false, ""},
+}
+
+func checkNotifierClusterMatch(cluster string) bool {
+	return cluster == "" || cluster == "testcluster"
 }
 
 func TestCoordinator_checkAndSendResponseToModules(t *testing.T) {
@@ -558,10 +563,15 @@ func TestCoordinator_checkAndSendResponseToModules(t *testing.T) {
 		// Set up the mock module and expected calls
 		mockModule := &helpers.MockModule{}
 		coordinator.modules["test"] = mockModule
+		mockModule.On("GetCluster").Return(testSet.Cluster)
+
+		if checkNotifierClusterMatch(testSet.Cluster) {
 		mockModule.On("GetName").Return("test")
 		mockModule.On("GetGroupAllowlist").Return((*regexp.Regexp)(nil))
 		mockModule.On("GetGroupDenylist").Return((*regexp.Regexp)(nil))
 		mockModule.On("AcceptConsumerGroup", response).Return(true)
+		}
+
 		if testSet.ExpectSend {
 			mockModule.On("Notify", response, mock.MatchedBy(func(s string) bool { return true }), mock.MatchedBy(func(t time.Time) bool { return true }), testSet.ExpectClose).Return()
 		}
